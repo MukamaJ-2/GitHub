1b3603235ddfa3851159f87cb6f4daefbec1568c
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-01-23 10:52:58 +0300

    fi

---
+++ HELLOSERVER/controlflows.js
@@ -0,0 +1,52 @@
+//if statement
+/** 
+if (condition) {
+    
+}
+else if(condition){
+
+}else{
+
+}
+*/
+
+
+
+
+let age = 20;
+
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
+
+
+//for loop
+/*
+for(intitialization, condition,increment){
+    results
+}*/
+
+for (i=1;i<=100;i = i+1){
+    console.log(i);
+}
+i=1
+while 
+(i<=100) {
+  console.log(i);
+  i = i+1;
+}
+
+//for
+let fruits = ["oranges", "grapes", "apples"]
+for (i in fruits){
+  console.log(fruits[i])
+}
+for (let i in fruits){
+  console.log(fruits[i])
+}
+
+for (let i of fruits){
+  console.log([i])
+}
