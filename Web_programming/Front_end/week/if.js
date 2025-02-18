19526b018c88d233d47e406ff46bec9c1602d509
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-02-18 17:26:39 +0300

    Create if.js

---
+++ Web_programming/Front_end/week/if.js
@@ -0,0 +1,12 @@
+let number = 5;
+if (number>0) {
+    console.log("it is a positive")
+}
+
+
+// example 2
+
+let isRaining = true;
+if (isRaining) {
+    console.log("Don't forget your umbrella")
+}
