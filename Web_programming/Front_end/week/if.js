d5002aef23bc42ac6d7cc94f4b23b42e380e337b
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-02-18 17:37:16 +0300

    Update if.js

--- Web_programming/Front_end/week/if.js
+++ Web_programming/Front_end/week/if.js
@@ -10,3 +10,26 @@ let isRaining = true;
 if (isRaining) {
     console.log("Don't forget your umbrella")
 }
+
+// example 3
+let age = 18;
+if (age>= 18){
+    console.log("You are eligible to vote")
+}
+
+//example 4
+if (number % 2 === 0) {
+    console.log("the number is even")
+} else{
+    console.log("the number is odd")
+}
+
+// example 5
+let score = 75; 
+if (score >= 80) { 
+    console.log("You got an A."); 
+} else if (score >= 60) { 
+    console.log("You got a B."); 
+} else { 
+    console.log("You need to improve."); 
+} 
