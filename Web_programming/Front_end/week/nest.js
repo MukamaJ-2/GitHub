ff304999e58ac8d130d0c776a4a78b4745503dc0
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-03-19 20:43:26 +0300

    Update nest.js

--- Web_programming/Front_end/week/nest.js
+++ Web_programming/Front_end/week/nest.js
@@ -9,18 +9,3 @@ if (number>=0) {
     console.log("The number is negative.");  
 }
 
-// exercise
-let expired = false;
-let quality = 7;
-if (expired ) {
-    console.log("Do not use");
-} else{
-    if (quality> 8){
-        console.log("Good Quality.");
-    } else if(quality>= 5 && quality<=8){
-        console.log("Average Quality.");
-    }else{
-        console.log("Poor Qaulity.");
-        
-    }
-}
