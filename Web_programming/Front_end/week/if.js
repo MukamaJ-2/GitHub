ae20033bbed80c33ff2ebe4ac7b0406df326cb4c
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-04-23 10:59:48 +0300

    Update if.js

--- Web_programming/Front_end/week/if.js
+++ Web_programming/Front_end/week/if.js
@@ -34,35 +34,3 @@ if (score >= 80) {
     console.log("You need to improve."); 
 } 
 
-// exercise 1
-let temp = 25;
-
-if (temp > 30) {
-  console.log("It's too hot");
-} else if (temp >= 20 && temp <= 30) {
-  console.log("It's moderate");
-} else if (temp < 20) {
-  console.log("It's cold.");
-}
-
-//exercise 2
-let scor = 55;
-if (scor>=70) {
-    console.log("Eligible for Admission.")
-}else if(scor>=50 && scor<=69){
-    console.log("Admission on probation.")
-}else if(scor<50){
-    console.log("Not Eligible for Admission.")
-}
-
-//exercise 3
-let waterAvailable = 300;
-if (waterAvailable>500) {
-    console.log("Enough water");
-    
-    
-}else if(waterAvailable>=200 && waterAvailable<= 500){
-    console.log("Water is limited");    
-}else if(waterAvailable<200){
-    console.log("Water shortage")
-}
