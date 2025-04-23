3c4227d7cecb9c409d8cff38f34b21ee53ee4a00
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-04-23 10:59:37 +0300

    Update loop.js

--- Web_programming/Front_end/week/loop.js
+++ Web_programming/Front_end/week/loop.js
@@ -12,20 +12,3 @@ for (let i = 1; i <= rows; i++) {
   console.log(pattern); 
 }
 
-// Exercise 5: Break at 7
-console.log("Break at 7");
-for (let i = 1; i <= 10; i++) {
-  if (i === 7) {
-    break; 
-  }
-  console.log(i);
-}
-
-// Exercise 6: Skip Even Numbers
-console.log("\nSkip Even Numbers");
-for (let i = 1; i <= 10; i++) {
-  if (i % 2 === 0) {
-    continue; 
-  }
-  console.log(i);
-}
